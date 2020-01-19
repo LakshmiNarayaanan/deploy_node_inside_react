aa499ed74c08d525d45b4fb726f4ebbad3abe6d8
LakshmiNarayaanan <lakshminarayaanan@gmail.com>
2020-01-19 13:26:43 +0530

    changed node env

--- server.js
+++ server.js
@@ -14,15 +14,14 @@ app.get('/checkthenode',(req,res) => {
   res.status(200).send("hurray you succedded")
 })
 
-// Serve static assets if in production
-if (process.env.NODE_ENV === 'production') {
+
   // Set static folder
   app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
-}
+
 
 const port = process.env.PORT || 5000;
 
